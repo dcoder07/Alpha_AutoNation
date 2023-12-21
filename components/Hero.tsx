8712d06dc59f3cfd26fe414273763d64cdb67e07
Dishank Patil <dishankjp05@gmail.com>
2023-12-21 13:13:27 +0530

    initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className='hero'>
+      <div className='pt-36 flex-1 padding-x'>
+        <h1 className='hero__title'>
+          Find, book, or rent a car <br /> — quickly and easily!
+        </h1>
+        <p className='hero__subtitle'>
+          Streamline your car rental experience with effortless booking process.
+        </p>
+      </div>
+    </section>
+  );
+}
